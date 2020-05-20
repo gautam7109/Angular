e756ffe8bb7d69e34af4f4420f4a24cae90b71f2
gautam7109 <47362834+gautam7109@users.noreply.github.com>
2020-05-21 02:43:30 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,92 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'BRAIN DUDEY';
+  address=" N.Daman Avenue,chicago 99999|999-999-9999|hello@kickresume|www.kickresume.com";
+  head1="Profile";
+  line1="Innovation optimized solution seeker.Excited to be";
+  line2="at the deployment phase of my new career as a";
+  line3=" we developer.I am ambitious, adventurous,";
+  line4="assiduous, animated, and an a lliteration advocate.";
+  head2="Skills";
+  subhead1="Technical Skills";
+  line5="JavaScript";
+  line6="CSS";
+  line7="HTML";
+  line8="React";
+  line9="Redux";
+  line10="Mongo";
+  line11="Deployment";
+  subhead2="Additional Skills";
+  line12=" Project Management";
+  line13="Recruitment";
+  line14="Business Development";
+  line15="Editing";
+  line16=" Fundraising";
+  head3="Work experience";
+  subhead3="Event Manager";
+  date1="03/2014-02/2017";
+  subhead31=" C3Presents. Washington DC";
+  line17="Lead and execute all phases of event planning";
+  line18="and production spanning committee";
+  line19="recruitment, training, vendor relationships and";
+  line20="on - site faciliation.";
+   line21="Brought new business to the organization ";
+   line22="through relentless networking and stewardship";
+   line23="which helped the company win the bid for the";
+   line24="State Department Summit on the Middle East";
+   line25="and, the companies largest civic event to date,";
+   line26=" the United State of Women";
+   line27="Exercise fiscal control over budget creation,";
+   line28="tracking and reporting.Collaborate with";
+   line29=" employees at all organizational levels to ";
+   line30="advance cohesive operations.";
+   head4="Work experience";
+   subhead4="Community Relations";
+   date2="06 / 2011 - 01 / 2014";
+   linea1=" Manager";
+   linea2="Gay & Lesbian Elder Housing, Los Angeles";
+   linea3="Arranging presentations and pitch deck.";
+   linea4="Designing a PR plan and establishing";
+   linea5="important focus points.";
+   linea6="Designing, creating and managing content";
+   linea7="across multiple communication platforms.";
+   linea8="Building relationships with key media players.";
+   head5="Education";
+   subdead5="Engineering Immersion";
+   date3="11 / 2018 - 06 / 2018";
+   subhead51="Program";
+   subhead52="Thinkful, Chicago, IL";
+   linea9="Project - focused intensive program with emphasis";
+   linea10="on Mongo, Express, React, and JavaScript(MERN)";
+   linea11=" technical stack.";
+   linea12="Developed a full - stack web application,";
+   linea13=" 'RenewU' using React that allows uses to";
+   linea14="explore various aspects of meditation.User's";
+   linea15="progress is stored on a backend created using";
+   linea16="Node and MongoDB.";
+   linea17="Developed a language learning app, 'Foodie";
+   linea18="phonetics' using spaced repetition and a linked";
+   linea19="list data structure.React was used to create";
+   linea20="the front end components while Node and";
+   linea21="Mongo were used to create a backend that";
+   linea22=" stores user data.";
+   linea23="Developed a concierge app, 'Pley', for";
+   linea24="individuals looking for curated suggestions";
+   linea25="when visiting a new place.React was used to";
+   linea26=" develop the front end which includes real - time";
+   linea27=" chat, drag and drop and variety of advanced.";
+   linea28=" features.The backend, built using Node,";
+   linea29="Express, and Mongo, takes advantage of well - ";
+   linea30="developed RESTful API, Geospatial searching";
+   linea31="and user authentication with JWT.";
+   subhead6="BA, English";
+   date4="09 / 2001 - 09 / 2005";
+   linea32="University of California, Los Angles.;..";
+
+}
